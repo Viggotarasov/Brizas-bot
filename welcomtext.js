a950e072fbdacbb187d54b9686d1ccfdaac6b5dc
VIGGO <95065212+Viggotarasov@users.noreply.github.com>
2022-02-22 10:42:02 -0500

    Update welcomtext.js

--- welcomtext.js
+++ welcomtext.js
@@ -1,9 +1,9 @@
 const welcome = (number, groupname) => {
-    return `𝐎𝐥𝐚 @${number}. 𝐒𝐞𝐣𝐚 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 ${groupname}`
+    return `Hola @${number}. Bienvenido a ${groupname}`
 }
 exports.welcome = welcome
 
 const bye = (number) => {
-    return `Adeus @${number}. Já vai tarde 👋`
+    return `Adiós @${number}. estaremos mejor sin tí 👋`
 }
 exports.bye = bye
